dfd3b0fad73898c45945b532d3a65625e391dfa3
eduarda-aires <eduardaaires@ua.pt>
2024-11-07 15:51:33 +0000

    added keywords, notes and images to photomoodboard

--- src/components/PhotoshootMoodboardTab.js
+++ src/components/PhotoshootMoodboardTab.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './PhotoshootMoodboardTab.css';
 
 const PhotoshootMoodboardTab = ({ moodboard, handleTextChange, handleImageChange }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openModal = (imgSrc) => {
+    setSelectedImage(imgSrc);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage('');
+  };
+
   return (
-    <div>
+    <div className="moodboard-container">
       <h3>Photoshoot Moodboard</h3>
+      
       <textarea
-        placeholder="Add photoshoot ideas..."
         value={moodboard.text}
         onChange={handleTextChange}
+        placeholder="Add keywords or inspiration notes here..."
+        className="moodboard-textarea"
       />
+      
       <input
         type="file"
         multiple
+        accept="image/*"
         onChange={handleImageChange}
+        style={{ marginTop: '10px' }}
       />
-      {moodboard.images && moodboard.images.map((img, idx) => (
-        <img key={idx} src={URL.createObjectURL(img)} alt="Moodboard" width="100" />
-      ))}
+
+      <div className="image-gallery">
+        {moodboard.images.map((imgSrc, index) => (
+          <img
+            key={index}
+            src={imgSrc}
+            alt={`Moodboard Inspiration ${index + 1}`}
+            onClick={() => openModal(imgSrc)}
+          />
+        ))}
+      </div>
+
+      {isModalOpen && (
+        <div className="modal" onClick={closeModal}>
+          <img src={selectedImage} alt="Selected" />
+        </div>
+      )}
     </div>
   );
 };
